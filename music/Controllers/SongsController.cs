d13258d80e60efb9cafc600c17921c02cdcdb84b
LuizFelipe <luizfelipers19@gmail.com>
2021-03-31 16:31:05 -0300

    including files

---
+++ music/Controllers/SongsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using music.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace music.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+
+        private List<Song> songs = new List<Song>()
+        {
+               new Song(){Id = 0, Title="Cabeça de Gelo", Language = "Português" } ,
+               new Song(){Id = 1, Title="Estilo Cachorro", Language = "Português" } ,
+           
+        };
+    }
+}

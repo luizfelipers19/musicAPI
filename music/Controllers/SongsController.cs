9113db29a4b4dd9b334eac83acb789a62995f718
LuizFelipe <luizfelipers19@gmail.com>
2021-03-31 22:59:08 -0300

    Hardcoded and working CRUD (PGPD), with hardcoded list of songs. No database created yet, and still without persistance

--- music/Controllers/SongsController.cs
+++ music/Controllers/SongsController.cs
@@ -13,11 +13,36 @@ namespace music.Controllers
     public class SongsController : ControllerBase
     {
 
-        private List<Song> songs = new List<Song>()
+        private static List<Song> songs = new List<Song>()
         {
-               new Song(){Id = 0, Title="Cabeça de Gelo", Language = "Português" } ,
-               new Song(){Id = 1, Title="Estilo Cachorro", Language = "Português" } ,
+            new Song(){Id = 0, Title="Cabeça de Gelo", Language = "Português" } ,
+            new Song(){Id = 1, Title="Estilo Cachorro", Language = "Português" } ,
            
         };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+
+            return songs;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Song song)
+        {
+            songs.Add(song);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Song song)
+        {
+            songs[id] = song;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            songs.RemoveAt(id);
+        }
     }
 }
